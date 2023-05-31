c0ca33907a7d48f0d559d4d96ee46a26135f4aa0
Дмитрий Нехороших <phantom_love02@mail.ru>
2023-05-31 17:56:26 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -1,7 +1,7 @@
-function sD() {
+function sd() {
     let out = document.getElementById('current-date');
     let today = new Date();
-    out.innerHTML="Дата и время для русскрй локали: "+ today.toLocaleDateString('ru-RU') + '<br>'; +
+    out.innerHTML="Дата и время для русскрй локали: "+ today.toLocaleDateString('ru-RU') + '<br>' +
     "Дата и время для греческой локали: " + today.toLocaleDateString('el-GR') + '<br>' +
     "Дата и время для китайской локали: " + today.toLocaleDateString('zh-CN') + '<br>' +
     "Дата и время для монгольской локали: " + today.toLocaleDateString('mn-MN') + '<br>' +
@@ -13,9 +13,9 @@ function sD() {
 }
 function sdc(){
     let today = new Date();
-    let inputDate = document.querySelector('input[type-date]');
+    let inputDate = document.querySelector('input[type=date]');
     let birthday = new Date(inputDate.value);
     let daysCount = (today - birthday)/1000/60/60/24;
     daysCount = Math.floor(daysCount);
-    alert(daysCount);
+    document.getElementById('rojd').innerHTML = "Количесвто дней с даты рождения: " + daysCount;
 }
